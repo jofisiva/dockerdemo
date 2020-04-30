3e08dbc749cbcff7201178ff4a4de943eb86c2af
smurugesan84 <sivabalan.murugesan@aimia.com>
2020-04-30 09:59:35 +0100

    Initial commit

---
+++ src/main/java/com/example/dockerdemo/DockerdemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DockerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerdemoApplication.class, args);
+	}
+
+}
+@RestController
+@RequestMapping("/docker")
+class DockerDemo {
+
+	@GetMapping
+	public String dockerDemo() {
+		return "Docker Demo Test Build";
+	}
+}
